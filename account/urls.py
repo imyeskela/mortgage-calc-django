57adb3be25a34239d6a9ccd92ecca5eca8cf914e
thisisfuckinalex <alxogloblin@gmail.com>
2022-07-27 18:38:55 +0300

    added view profile(with his mortgages)

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('', ),
+    path('<user_id>/', views.UserApiView.as_view()),
     path('registration/', views.RegistrationApiView.as_view()),
     path('auth/', include('rest_framework.urls'))
     # path('login/', ),
--- account/views.py
+++ account/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
+from rest_framework.response import Response
 from rest_framework.viewsets import generics
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
 from .models import User
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer, UserSerializer
 
 
 class RegistrationApiView(generics.CreateAPIView):
@@ -15,4 +18,11 @@ class RegistrationApiView(generics.CreateAPIView):
         return HttpResponseRedirect('http://127.0.0.1:8000/api/v1/account/auth/login/')
 
 
-
+class UserApiView(APIView):
+    def get(self, request, *args, **kwargs):
+        if str(kwargs['user_id']) == str(request.user.pk):
+            user = get_object_or_404(User, pk=kwargs['user_id'])
+            serializer = UserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response({'error': 'oops ('})
